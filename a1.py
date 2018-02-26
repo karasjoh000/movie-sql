5581d8cb605dd624275f372e38699411f48404fd
John Karasev <john.karasev@wsu.edu>
2018-02-26 08:18:06 -0800

    add some more schemas

--- a1.py
+++ a1.py
@@ -4,6 +4,7 @@ import json
 
 
 def create_tables(cur):
+
     cur.execute('CREATE TABLE movie('
                 'id INT,'
                 'budget INT,'
@@ -12,7 +13,7 @@ def create_tables(cur):
                 'homepage TEXT, '
                 'overview TEXT, '
                 'popularity DOUBLE, '
-                'release_date DATE, '
+                'release_date VARCHAR(20), '
                 'revenue DOUBLE, '
                 'runtime INT, '
                 'status VARCHAR(10),'
@@ -37,7 +38,8 @@ def create_tables(cur):
 
     cur.execute('CREATE TABLE keywords('
                 'id INT, '
-                'keyword INT)')
+                'keyword VARCHAR(50), '
+                'PRIMARY KEY(id))')
 
     cur.execute('CREATE TABLE movie_keywords('
                 'id INT, '
@@ -49,7 +51,8 @@ def create_tables(cur):
 
     cur.execute('CREATE TABLE production_companies('
                 'id INT, '
-                'company VARCHAR(100))')
+                'company VARCHAR(100), '
+                'PRIMARY KEY(id))')
 
     cur.execute('CREATE TABLE movie_production_companies('
                 'id INT, '
@@ -59,6 +62,7 @@ def create_tables(cur):
                 'FOREIGN KEY(movie_id) REFERENCES movie(id),'
                 'FOREIGN KEY(company) REFERENCES production_companies(id))')
 
+
 def open_csv(path):
     movies = open(path, 'r')
     return csv.DictReader(movies)
@@ -67,24 +71,29 @@ def open_csv(path):
 def update_table(row, cur):
     # update genre table
     for genre in json.loads(row['genres']):
-        print('{0}, {1}'.format(str(genre['id']), str(genre['name'])))
         cur.execute('INSERT INTO genre(id, name) '
                     'VALUES({0}, \'{1}\') '
                     'ON DUPLICATE KEY UPDATE id=id'.format(str(genre['id']), str(genre['name'])))
 
-    cur.execute('INSERT INTO movie()')
+    cur.execute(
+        'INSERT INTO movie VALUES({0}, {1}, \'{2}\', \'{3}\', \'{4}\', \'{5}\', '
+        '{6}, {7}, {8}, {9}, {10}, {11}, ''{12}, {13}, {14})'.format(
+            row['id'], row['budget'], row['original_title'], row['original_language'], row['homepage'], row['overview'],
+            row['popularity'], row['release_date'], row['revenue'], row['runtime'], row['status'], row['tagline'],
+            row['title'], row['vote_average'], row['vote_count']))
 
 
 def main():
     conn = pymysql.connect(host='127.0.0.1', port=3306, user='john', passwd='Jkarasev37', db='movies')
     cur = conn.cursor()
 
-    # create_tables(cur)
+    create_tables(cur)
 
     reader = csv.DictReader(open('tmdb_5000_movies.csv'))
 
     for row in reader:
         update_table(row, cur)
+
     conn.commit()
 
     conn.close()
