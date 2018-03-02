622fbfead782ee25188423210df76c3895b12847
John I Karasev <john.karasev@wsu.edu>
2018-03-02 10:22:09 -0800

    add country and formate date

--- a1.py
+++ a1.py
@@ -1,7 +1,8 @@
 import pymysql
 import csv
 import json
-import binascii as bn
+
+
 
 
 def create_tables(cur):
@@ -13,7 +14,7 @@ def create_tables(cur):
                 'homepage TEXT, '
                 'overview TEXT, '
                 'popularity DOUBLE, '
-                'release_date VARCHAR(20), '
+                'release_date DATE, '
                 'revenue DOUBLE, '
                 'runtime INT, '
                 'status VARCHAR(50),'
@@ -62,6 +63,20 @@ def create_tables(cur):
                 'FOREIGN KEY(movie_id) REFERENCES movie(id),'
                 'FOREIGN KEY(company) REFERENCES production_companies(id))')
 
+    cur.execute('CREATE TABLE production_countries('
+                'iso_3166_1 VARCHAR(2), '
+                'cname VARCHAR(50), '
+                'PRIMARY KEY(iso_3166_1))')
+
+    cur.execute('CREATE TABLE movie_production_countries('
+                'id INT AUTO_INCREMENT, '
+                'movie_id INT, '
+                'iso_3166_1 VARCHAR(2), '
+                'PRIMARY KEY(id), '
+                'FOREIGN KEY(movie_id) REFERENCES movie(id), '
+                'FOREIGN KEY(iso_3166_1) REFERENCES production_countries(iso_3166_1))')
+
+
 
 def open_csv(path):
     movies = open(path, 'r')
@@ -69,12 +84,21 @@ def open_csv(path):
 
 
 def str_format(text):
+    # return '"' + str(text.encode('ascii', 'ignore'), 'ascii').replace('"', '\\"') + '"'
     return '"' + str(text.encode('ascii', 'ignore'), 'ascii').replace('"', '\\"') + '"'
+
+
+def date_format(date):
+    if date == "":
+        return 'NULL'
+    elif date is None:
+        return 'NULL'
+    else:
+        return "'" + date + "'"
 
 
 def check_empty(value):
     if value is None:
-        print("Found None")
         return 'NULL'
     elif value == "":
         return 'NULL'
@@ -88,7 +112,7 @@ def update_table(row, cur):
         '{6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14})'.format(
             row['id'], row['budget'], str_format(row['original_title']), str_format(row['original_language']),
             str_format(row['homepage']), str_format(row['overview']), row['popularity'],
-            str_format(row['release_date']), row['revenue'], check_empty(row['runtime']), str_format(row['status']),
+            date_format(row['release_date']), row['revenue'], check_empty(row['runtime']), str_format(row['status']),
             str_format(row['tagline']), str_format(row['title']), row['vote_average'], row['vote_count']))
 
     for genre in json.loads(row['genres']):
@@ -118,13 +142,19 @@ def update_table(row, cur):
         cur.execute('INSERT INTO movie_production_companies(movie_id, company)'
                     'VALUE({0}, {1})'.format(row['id'], company['id']))
 
+    for country in json.loads(row['production_countries']):
+
+        cur.execute('INSERT INTO production_countries(iso_3166_1, cname) '
+                    'VALUE({0}, {1})'
+                    'ON DUPLICATE KEY UPDATE iso_3166_1=iso_3166_1'.format(str_format(country['iso_3166_1']), str_format(country['name'])))
+
+        cur.execute('INSERT INTO movie_production_countries(movie_id, iso_3166_1)'
+                    'VALUE({0}, {1})'.format(row['id'], str_format(country['iso_3166_1'])))
 
 def main():
-    print("in main")
+
     conn = pymysql.connect(host='127.0.0.1', port=3306, user='john', passwd='Jkarasev37', db='movies', )
     cur = conn.cursor()
-
-    conn.commit()
 
     create_tables(cur)
 
