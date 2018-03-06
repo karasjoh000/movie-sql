d38b8ddb124493216f56502fdf05a2879ab4f3bd
John I Karasev <john.karasev@wsu.edu>
2018-03-06 14:26:22 -0800

    add spoken langauges tables

--- src/a1_p1.py
+++ src/a1_p1.py
@@ -2,12 +2,12 @@ import pymysql
 import csv
 import json
 
-# TODO: Create Spoken_languages TABLE, AND CHECK THAT ALL CSV ROWS ARE IN DATABASE
+
+# TODO: CHECK THAT ALL CSV ROWS ARE IN DATABASE!!!!
 
 
 # create the empty tables
 def create_tables(cur):
-
     # stores all columns that where atomic in the csv
     cur.execute('CREATE TABLE movie('
                 'id INT,'
@@ -84,6 +84,19 @@ def create_tables(cur):
                 'FOREIGN KEY(movie_id) REFERENCES movie(id), '
                 'FOREIGN KEY(iso_3166_1) REFERENCES production_countries(iso_3166_1))')
 
+    cur.execute('CREATE TABLE spoken_languages('
+                'iso_639_1 VARCHAR(2), '
+                'name VARCHAR(50), '
+                'PRIMARY KEY(iso_639_1))')
+
+    cur.execute('CREATE TABLE movie_spoken_languages('
+                'id INT AUTO_INCREMENT, '
+                'movie_id INT, '
+                'iso_639_1 VARCHAR(2), '
+                'PRIMARY KEY(id), '
+                'FOREIGN KEY(movie_id) REFERENCES movie(id), '
+                'FOREIGN KEY(iso_639_1) REFERENCES spoken_languages(iso_639_1))')
+
 
 # format the string values to be stored.
 def str_format(text):
@@ -112,7 +125,6 @@ def check_empty(value):
 
 # update each time a row is received from the csv.
 def update_table(row, cur):
-
     # update the movies table with it's corresponding values.
     cur.execute(
         'INSERT INTO movie VALUE({0}, {1}, {2}, {3}, {4}, {5}, '
@@ -158,24 +170,35 @@ def update_table(row, cur):
         # update production_countries table on duplicates to not store a country more than once
         cur.execute('INSERT INTO production_countries(iso_3166_1, cname) '
                     'VALUE({0}, {1})'
-                    'ON DUPLICATE KEY UPDATE iso_3166_1=iso_3166_1'.format(str_format(country['iso_3166_1']), str_format(country['name'])))
+                    'ON DUPLICATE KEY UPDATE iso_3166_1=iso_3166_1'.format(str_format(country['iso_3166_1']),
+                                                                           str_format(country['name'])))
         # update movie_production_countries with foreign keys to show what country belong
         # to the movie in the current row.
         cur.execute('INSERT INTO movie_production_countries(movie_id, iso_3166_1)'
                     'VALUE({0}, {1})'.format(row['id'], str_format(country['iso_3166_1'])))
 
+    for language in json.loads(row['spoken_languages']):
+        # update spoken_languages table on duplicates to not store a country more than once
+        cur.execute('INSERT INTO spoken_languages(iso_639_1, name) '
+                    'VALUE({0}, {1})'
+                    'ON DUPLICATE KEY UPDATE iso_639_1=iso_639_1'.format(str_format(language['iso_639_1']),
+                                                                           str_format(language['name'])))
+        # update movie_spoken_languages with foreign keys to show what languages belong
+        # to the movie in the current row.
+        cur.execute('INSERT INTO movie_spoken_languages(movie_id, iso_639_1)'
+                    'VALUE({0}, {1})'.format(row['id'], str_format(language['iso_639_1'])))
+
 
 def main():
-
     # connect to the database
     conn = pymysql.connect(host='127.0.0.1', port=3306, user='john', passwd='Jkarasev37', db='movies', )
     cur = conn.cursor()
 
     # create empty tables
-    create_tables(cur)
+    # create_tables(cur)
 
     # open csv reader
-    reader = csv.DictReader(open('tmdb_5000_movies.csv', encoding='utf-8'))
+    reader = csv.DictReader(open('src/tmdb_5000_movies.csv', encoding='utf-8'))
 
     # for each row in csv, update database
     for row in reader:
