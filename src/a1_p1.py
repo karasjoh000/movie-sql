6878d961e5d3232557343a7eb5cc2b5215f70897
John I Karasev <john.karasev@wsu.edu>
2018-03-07 16:36:07 -0800

    change cur.exec statements and change text to utfmb3

--- src/a1_p1.py
+++ src/a1_p1.py
@@ -12,20 +12,21 @@ def create_tables(cur):
     cur.execute('CREATE TABLE movie('
                 'id INT,'
                 'budget INT,'
-                'original_title TEXT, '
+                'original_title TEXT CHARACTER SET utf8mb3, '
                 'original_language VARCHAR(2), '
                 'homepage TEXT, '
                 'overview TEXT, '
                 'popularity DOUBLE, '
                 'release_date DATE, '
                 'revenue DOUBLE, '
-                'runtime INT, '
+                'runtime DOUBLE, '
                 'status VARCHAR(50),'
                 'tagline TEXT, '
                 'title VARCHAR(100), '
                 'vote_average FLOAT, '
                 'vote_count DOUBLE, '
-                'PRIMARY KEY(id))')
+                'PRIMARY KEY(id))'
+                'CHARACTER SET utf8mb3')
 
     # all columns that where not atomic, where split into two tables.
     # One table was used to store specific information like genres and their id's
@@ -35,7 +36,8 @@ def create_tables(cur):
     cur.execute('CREATE TABLE genre('
                 'id INT,'
                 'name VARCHAR(50), '
-                'PRIMARY KEY(id))')
+                'PRIMARY KEY(id))'
+                'CHARACTER SET utf8mb3')
 
     cur.execute('CREATE TABLE movie_genre('
                 'id INT AUTO_INCREMENT,'
@@ -43,12 +45,14 @@ def create_tables(cur):
                 'genre_id INT,'
                 'PRIMARY KEY(id),'
                 'FOREIGN KEY(movie_id) REFERENCES movie(id),'
-                'FOREIGN KEY(genre_id) REFERENCES genre(id))')
+                'FOREIGN KEY(genre_id) REFERENCES genre(id))'
+                'CHARACTER SET utf8mb3')
 
     cur.execute('CREATE TABLE keywords('
                 'id INT, '
                 'keyword VARCHAR(50), '
-                'PRIMARY KEY(id))')
+                'PRIMARY KEY(id))'
+                'CHARACTER SET utf8mb3')
 
     cur.execute('CREATE TABLE movie_keywords('
                 'id INT AUTO_INCREMENT, '
@@ -56,12 +60,14 @@ def create_tables(cur):
                 'keyword_id INT, '
                 'PRIMARY KEY(id), '
                 'FOREIGN KEY(movie_id) REFERENCES movie(id), '
-                'FOREIGN KEY (keyword_id) REFERENCES keywords(id))')
+                'FOREIGN KEY (keyword_id) REFERENCES keywords(id))'
+                'CHARACTER SET utf8mb3')
 
     cur.execute('CREATE TABLE production_companies('
                 'id INT, '
                 'company VARCHAR(100), '
-                'PRIMARY KEY(id))')
+                'PRIMARY KEY(id))'
+                'CHARACTER SET utf8mb3')
 
     cur.execute('CREATE TABLE movie_production_companies('
                 'id INT AUTO_INCREMENT, '
@@ -69,12 +75,14 @@ def create_tables(cur):
                 'company_id INT,'
                 'PRIMARY KEY(id),'
                 'FOREIGN KEY(movie_id) REFERENCES movie(id),'
-                'FOREIGN KEY(company_id) REFERENCES production_companies(id))')
+                'FOREIGN KEY(company_id) REFERENCES production_companies(id))'
+                'CHARACTER SET utf8mb3')
 
     cur.execute('CREATE TABLE production_countries('
                 'iso_3166_1 VARCHAR(2), '
                 'cname VARCHAR(50), '
-                'PRIMARY KEY(iso_3166_1))')
+                'PRIMARY KEY(iso_3166_1))'
+                'CHARACTER SET utf8mb3')
 
     cur.execute('CREATE TABLE movie_production_countries('
                 'id INT AUTO_INCREMENT, '
@@ -82,123 +90,123 @@ def create_tables(cur):
                 'iso_3166_1 VARCHAR(2), '
                 'PRIMARY KEY(id), '
                 'FOREIGN KEY(movie_id) REFERENCES movie(id), '
-                'FOREIGN KEY(iso_3166_1) REFERENCES production_countries(iso_3166_1))')
+                'FOREIGN KEY(iso_3166_1) REFERENCES production_countries(iso_3166_1))'
+                'CHARACTER SET utf8mb3')
 
     cur.execute('CREATE TABLE spoken_languages('
                 'iso_639_1 VARCHAR(2), '
                 'name VARCHAR(50), '
-                'PRIMARY KEY(iso_639_1))')
+                'PRIMARY KEY(iso_639_1))'
+                'CHARACTER SET utf8mb3')
 
     cur.execute('CREATE TABLE movie_spoken_languages('
                 'id INT AUTO_INCREMENT, '
                 'movie_id INT, '
-                'iso_639_1 VARCHAR(2), '
+                'iso_639_1 VARCHAR(10), '
                 'PRIMARY KEY(id), '
                 'FOREIGN KEY(movie_id) REFERENCES movie(id), '
-                'FOREIGN KEY(iso_639_1) REFERENCES spoken_languages(iso_639_1))')
-
-
-# format the string values to be stored.
-def str_format(text):
-    return '"' + str(text.encode('ascii', 'ignore'), 'ascii').replace('"', '\\"') + '"'
-
-
-# if date is an empty string return 'NULL'
-def date_format(date):
-    if date == "":
-        return 'NULL'
-    elif date is None:
-        return 'NULL'
-    else:
-        return "'" + date + "'"
-
-
-# if value is empty string or None, return 'NULL'
-def check_empty(value):
-    if value is None:
-        return 'NULL'
-    elif value == "":
-        return 'NULL'
-    else:
-        return value
+                'FOREIGN KEY(iso_639_1) REFERENCES spoken_languages(iso_639_1))'
+                'CHARACTER SET utf8mb3')
 
 
 # update each time a row is received from the csv.
 def update_table(row, cur):
     # update the movies table with it's corresponding values.
     cur.execute(
-        'INSERT INTO movie VALUE({0}, {1}, {2}, {3}, {4}, {5}, '
-        '{6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14})'.format(
-            row['id'], row['budget'], str_format(row['original_title']), str_format(row['original_language']),
-            str_format(row['homepage']), str_format(row['overview']), row['popularity'],
-            date_format(row['release_date']), row['revenue'], check_empty(row['runtime']), str_format(row['status']),
-            str_format(row['tagline']), str_format(row['title']), row['vote_average'], row['vote_count']))
+        'INSERT INTO movie VALUE(%s, %s, %s, %s, %s, %s, '
+        '%s, %s, %s, %s, %s, %s, %s, %s, %s)',
+        list(map(lambda x: x if x else None, [row['id'], row['budget'], row['original_title'],
+                                              row['original_language'], row['homepage'],
+                                              row['overview'], row['popularity'],
+                                              row['release_date'], row['revenue'],
+                                              row['runtime'],
+                                              row['status'], row['tagline'], row['title'],
+                                              row['vote_average'], row['vote_count']])))
 
     # for each genre in csv row, update the movie_genre table and genre table.
     for genre in json.loads(row['genres']):
         # update genre table on duplicate to not store genre more than once
         cur.execute('INSERT INTO genre(id, name) '
-                    'VALUE({0}, {1}) '
-                    'ON DUPLICATE KEY UPDATE id=id'.format(genre['id'], str_format(genre['name'])))
+                    'VALUE(%s, %s) '
+                    'ON DUPLICATE KEY UPDATE id=id',
+                    list(map(lambda x: x if x else None, [genre['id'], genre['name']])))
+
         # update movie_genre with foreign keys to show what genres belong to the movie in the current row.
         cur.execute('INSERT INTO movie_genre(movie_id, genre_id) '
-                    'VALUE({0}, {1})'.format(row['id'], genre['id']))
+                    'VALUE(%s, %s)', list(map(lambda x: x if x else None, [row['id'], genre['id']])))
 
-    # for each keyword in csv row, update the movie_keywords and keywords table.
+        # for each keyword in csv row, update the movie_keywords and keywords table.
     for keyword in json.loads(row['keywords']):
         # update keywords table on duplicates to not store a keyword more than once
         cur.execute('INSERT INTO keywords(id, keyword)'
-                    'VALUE({0}, {1})'
-                    'ON DUPLICATE KEY UPDATE id=id'.format(keyword['id'], str_format(keyword['name'])))
+                    'VALUE(%s, %s)'
+                    'ON DUPLICATE KEY UPDATE id=id',
+                    list(map(lambda x: x if x else None, [keyword['id'], keyword['name']])))
+
         # update movie_genre with foreign keys to show what genres belong to the movie in the current row. .
         cur.execute('INSERT INTO movie_keywords(movie_id, keyword_id) '
-                    'VALUE({0}, {1})'.format(row['id'], keyword['id']))
+                    'VALUE(%s, %s)', list(map(lambda x: x if x else None, [row['id'], keyword['id']])))
 
-    # for each company in csv row, update the movie_production_companies and production_companies table.
+        # for each company in csv row, update the movie_production_companies and production_companies table.
     for company in json.loads(row['production_companies']):
         # update production_companies table on duplicates to not store a company more than once
         cur.execute('INSERT INTO production_companies(id, company)'
-                    'VALUES({0}, {1})'
-                    'ON DUPLICATE KEY UPDATE id=id'.format(company['id'], str_format(company['name'])))
+                    'VALUES(%s, %s)'
+                    'ON DUPLICATE KEY UPDATE id=id',
+                    list(map(lambda x: x if x else None, [company['id'], company['name']])))
+
         # update movie_production_companies with foreign keys to show what companies belong
         # to the movie in the current row.
         cur.execute('INSERT INTO movie_production_companies(movie_id, company_id)'
-                    'VALUE({0}, {1})'.format(row['id'], company['id']))
+                    'VALUE(%s, %s)', list(map(lambda x: x if x else None, [row['id'], company['id']])))
 
-    # for each company in csv row, update the movie_production_countries and production_countries table.
+        # for each company in csv row, update the movie_production_countries and production_countries table.
     for country in json.loads(row['production_countries']):
         # update production_countries table on duplicates to not store a country more than once
         cur.execute('INSERT INTO production_countries(iso_3166_1, cname) '
-                    'VALUE({0}, {1})'
-                    'ON DUPLICATE KEY UPDATE iso_3166_1=iso_3166_1'.format(str_format(country['iso_3166_1']),
-                                                                           str_format(country['name'])))
+                    'VALUE(%s, %s)'
+                    'ON DUPLICATE KEY UPDATE iso_3166_1=iso_3166_1',
+                    list(map(lambda x: x if x else None, [country['iso_3166_1'], country['name']])))
         # update movie_production_countries with foreign keys to show what country belong
         # to the movie in the current row.
         cur.execute('INSERT INTO movie_production_countries(movie_id, iso_3166_1)'
-                    'VALUE({0}, {1})'.format(row['id'], str_format(country['iso_3166_1'])))
+                    'VALUE(%s, %s)', list(map(lambda x: x if x else None, [row['id'], country['iso_3166_1']])))
 
     for language in json.loads(row['spoken_languages']):
+
         # update spoken_languages table on duplicates to not store a country more than once
         cur.execute('INSERT INTO spoken_languages(iso_639_1, name) '
-                    'VALUE({0}, {1})'
-                    'ON DUPLICATE KEY UPDATE iso_639_1=iso_639_1'.format(str_format(language['iso_639_1']),
-                                                                           str_format(language['name'])))
+                    'VALUE(%s, %s)'
+                    'ON DUPLICATE KEY UPDATE iso_639_1=iso_639_1',
+                    list(map(lambda x: x if x else None, [language['iso_639_1'],
+                                                          language['name']])))
+
         # update movie_spoken_languages with foreign keys to show what languages belong
         # to the movie in the current row.
         cur.execute('INSERT INTO movie_spoken_languages(movie_id, iso_639_1)'
-                    'VALUE({0}, {1})'.format(row['id'], str_format(language['iso_639_1'])))
+                    'VALUE(%s, %s)',
+                    list(map(lambda x: x if x else None, [row['id'], language['iso_639_1']])))
 
 
 def main():
     # connect to the database
-    conn = pymysql.connect(host='127.0.0.1', port=3306, user='john', passwd='Jkarasev37', db='movies', )
+    conn = pymysql.connect(host='127.0.0.1', port=3306, user='john', passwd='Jkarasev37', db='movies',
+                           charset='utf8mb4')
     cur = conn.cursor()
 
     # create empty tables
-    # create_tables(cur)
+    create_tables(cur)
+
+    # set to 3 byte unicode
+
+#    cur.execute("SET NAMES 'utf8mb3'")
+#    cur.execute("SET CHARACTER SET utf8mb3")
+#    cur.execute("SET character_set_connection=utf8mb3")
 
     # open csv reader
     reader = csv.DictReader(open('src/tmdb_5000_movies.csv', encoding='utf-8'))
+
+    # cur.execute('ALTER DATABASE movies CHARACTER SET utf8 COLLATE utf8_general_ci')
 
     # for each row in csv, update database
     for row in reader:
